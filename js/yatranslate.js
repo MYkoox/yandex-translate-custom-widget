68dab8d489fb99980c9d0776eedeff74cc4462f5
Maxim <115556549+MYkoox@users.noreply.github.com>
2023-01-19 11:07:00 +0300

    Update yatranslate.js

--- js/yatranslate.js
+++ js/yatranslate.js
@@ -67,7 +67,7 @@ function yaTranslateGetCode() {
 function yaTranslateHtmlHandler(code) {
     // Получаем язык на который переводим и производим необходимые манипуляции с DOM
     // We get the language to which we translate and produce the necessary manipulations with DOM 
-    document.querySelector('[data-lang-active]').innerHTML = `<img class="lang__img lang__img_select" src="./images/lang/lang__${code}.png" alt="${code}">`;
+    document.querySelector('[data-lang-active]').innerHTML = `<img class="lang__img lang__img_select" src="https://raw.githubusercontent.com/mykoox/yandex-translate-custom-widget/main/images/lang/lang__${code}.png" alt="${code}">`;
     document.querySelector(`[data-ya-lang="${code}"]`).remove();
 }
 
